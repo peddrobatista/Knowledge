ce4f78ed1f14e93f1f0cb7a408019bf3d90a346a
pedro-batista <peddrobatista2022@gmail.com>
2023-05-08 15:15:42 -0300

    Atualizando projeto...

--- versao-inicial/backend/index.js
+++ versao-inicial/backend/index.js
@@ -1,4 +1,14 @@
 const app = require('express')();
+const consign = require('consign');
+const config = require('./config/db');
+
+app.db = db;
+
+consign()
+.then('./config/middlewares.js')
+.then('./api')
+.then('./config/routes.js')
+.into(app)
 
 app.listen(3000, () => {
     console.log('Backend executando...');
