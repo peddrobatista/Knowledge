08de01719b6425b4d8fdde4f6b816e3d055923b1
pedro-batista <peddrobatista2022@gmail.com>
2023-05-10 14:03:03 -0300

    Atualizando projeto...

--- versao-inicial/backend/api/user.js
+++ versao-inicial/backend/api/user.js
@@ -1,6 +1,38 @@
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = app => {
-    const save = (req, res) => {
-        res.send('user save')
+    const {existsOrError, notExistsOrError, equalsOrError} = app.api.validation
+
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+        return bcrypt.hashSync(password, salt)
+    }
+
+    async function save(req, res) {
+        const user = {...req.body}
+        if (req.params.id) user.id = req.params.id
+
+        try {
+            existsOrError(user.name, 'Nome não informado!')
+            existsOrError(user.email, 'Email não informado!')
+            existsOrError(user.password, 'Senha não informada!')
+            existsOrError(user.confirmPassword, 'Confirmação de senha inválida!')
+            equalsOrError(user.password, user.confirmPassword, 'Senhas não conferem!')
+
+            const userFromDB = await app.db('users').where({email: user.email}).first()
+            if(user.id) {
+                notExistsOrError(userFromDB, 'Usuário já cadastrado!')
+            }
+        } catch(msg) {
+            return res.status(400).send(msg)
+        }
+
+        user.password = encryptPassword(user.password)
+        delete user.confirmPassword
+
+        if(!user.id) {
+            app.db('users').update
+        }
     }
     return {save}
 }
