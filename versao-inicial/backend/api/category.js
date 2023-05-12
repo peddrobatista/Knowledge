4ea4ac596febd8616e0cd2c56ef609b779e73120
pedro-batista <peddrobatista2022@gmail.com>
2023-05-12 15:32:34 -0300

    atualizando projeto...

--- versao-inicial/backend/api/category.js
+++ versao-inicial/backend/api/category.js
@@ -49,9 +49,61 @@ module.exports = app => {
 
         const withPath = categories => {
             const getParent = (categories, parentId) => {
-                let parent = categories.filter(parent => parent.id === parentId)
-                return parent.length ? parent[0]
+                const parent = categories.filter(parent => parent.id === parentId)
+                return parent.length ? parent[0] : null
             }
+
+            const categoriesWithPath = categories.map(category => {
+                let path = category.name
+                let parent = getParent(categories, category.parentId)
+
+                while(parent) {
+                    path = `${parent.name} > ${path}`
+                    parent = getParent(categories, parent.parentId)
+                }
+
+                return {...category, path}
+            })
+
+            categoriesWithPath.sort((a, b) => {
+                if(a.path < b.path) return -1
+                if(a.path > b.path) return 1
+                return 0
+            })
+
+            return categoriesWithPath
         }
+
+        const get = (req, res) => {
+            app.db('categories')
+            .then(categories => res.json(withPath(categories)))
+            .catch(err => res.status(500).send(err))
+        }
+
+        const getById = (req, res) => {
+            app.db('categories')
+            .where({id: req.params.id})
+            .first()
+            .then(category => res.json(category))
+            .catch(err => res.status(500).send(err))
+        }
+
+        const toTree = (categories, tree) => {
+            if(!tree) tree = categories.filter(c = !c.parentId)
+            tree = tree.map(parentNode => {
+                const isChild = node => node.parentId == parentNode.id
+                parentNode.children = toTree(categories, categories.filter(isChild))
+                return parentNode
+            })
+            return tree
+        }
+
+        const getTree = (req, res) => {
+            app.db('categories')
+            .then(categories => res.json(withPath(categories)))
+            .catch(err => res.status(500).send(err))
+        }
+
+        return {save, remove, get, getById, getTree}
     }
 }
