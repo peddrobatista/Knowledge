5665d9c06cf952d8b73448753f9ca4d564da6140
pedro-batista <peddrobatista2022@gmail.com>
2023-05-23 14:47:01 -0300

    Atualizando componentes...

--- versao-inicial/frontend/src/App.vue
+++ versao-inicial/frontend/src/App.vue
@@ -1,15 +1,42 @@
 <template>
 	<div id="app">
-		<h1>Versão Inicial</h1>
+		<Header title="Cod3r - Base de Conhecimento" :hideToggle="true"/>
+		<Menu/>
+		<Content/>
+		<Footer/>
 	</div>
 </template>
 
 <script>
+import Header from "@/components/template/Header";
+import Menu from "@/components/template/Menu";
+import Content from "@/components/template/Content.vue";
+import Footer from "@/components/template/Footer.vue";
+
 export default {
 	name: "App",
+	components: {Header, Menu, Content, Footer}
 }
 </script>
 
 <style>
-
+	* {
+		font-family: "Lato", sans-serif;
+	}
+	body {
+		margin: 0;
+	}
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas: 
+			"header header"
+			"menu content"
+			"menu footer";
+		
+	}
 </style>
