6871d471b5aab95ae7a4a87cba29a02565dbe1d5
pedro-batista <peddrobatista2022@gmail.com>
2023-05-24 15:47:29 -0300

    Atualizando projeto...

--- versao-inicial/frontend/src/components/template/Header.vue
+++ versao-inicial/frontend/src/components/template/Header.vue
@@ -6,15 +6,20 @@
     <h1 class="title">
         {{ title }}
     </h1>
+    <UserDropdown v-if="!hideUserDropdown"/>
   </header>
 </template>
 
 <script>
+import UserDropdown from './UserDropdown.vue';
+
 export default {
     name: 'Header',
+    components: {UserDropdown},
     props: {
         title: String,
-        hideToggle: Boolean
+        hideToggle: Boolean,
+        hideUserDropdown: Boolean
     },
     computed: {
         icon() {
@@ -59,6 +64,7 @@ export default {
         align-items: center;
     }
     .header.header > a.toggle:hover {
+        color: #fff;
         background-color: rgba(0, 0, 0, 0.2);
     }
 </style>
