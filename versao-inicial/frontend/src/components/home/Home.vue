b2198fe3076ff0c25f3c24a2fe28deb6bdfdc2ae
pedro-batista <peddrobatista2022@gmail.com>
2023-05-25 14:57:31 -0300

    Atualizando projeto...

--- versao-inicial/frontend/src/components/home/Home.vue
+++ versao-inicial/frontend/src/components/home/Home.vue
@@ -2,6 +2,14 @@
     <div class="home">
         <PageTitle icon="fa fa-home" main="Dashboard"
         sub="Base de Conhecimento"/>
+        <div class="stats">
+            <Stat title="Categorias" :value="stat.categories"
+            icon="fa fa-folder" color="#d54d50"/>
+            <Stat title="Artigos" :value="stat.articles"
+            icon="fa fa-file" color="#3bc480"/>
+            <Stat title="Usuários" :value="stat.users"
+            icon="fa fa-user" color="#3282cd"/>
+        </div>
     </div>
 </template>
 
@@ -21,12 +29,17 @@ export default {
     },
     methods: {
         getStats() {
-            axios.get(`${baseApiUrl}/stats`)
+            axios.get(`${baseApiUrl}/stats`).then(res => this.stat = res.data)
         }
+    },
+    mounted() {
+        this.getStats()
     }
 }
 </script>
 
 <style>
-
+    .stat {
+        
+    }
 </style>
