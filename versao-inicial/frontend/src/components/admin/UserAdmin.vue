b3486b60feff29de307bc5b62131476655c87649
pedro-batista <peddrobatista2022@gmail.com>
2023-05-29 10:29:00 -0300

    Atualizando projeto...

--- versao-inicial/frontend/src/components/admin/UserAdmin.vue
+++ versao-inicial/frontend/src/components/admin/UserAdmin.vue
@@ -7,6 +7,7 @@
                     <b-form-group label="Nome:" label-for="user-name">
                         <b-form-input id="user-name" type="text"
                             v-model="usr.name" required 
+                            :readonly="mode === 'remove'"
                             placeholder="Informe o nome do usuário..."/>
                     </b-form-group>
                 </b-col>
@@ -14,12 +15,16 @@
                     <b-form-group label="E-mail:" label-for="user-email">
                         <b-form-input id="user-email" type="text"
                             v-model="usr.email" required 
+                            :readonly="mode === 'remove'"
                             placeholder="Informe o E-mail do usuário..."/>
                     </b-form-group>
                 </b-col>
             </b-row>
-            <b-form-checkbox id="user-admin" v-model="user.admin"/>
-            <b-row>
+            <b-form-checkbox id="user-admin" v-show="mode === 'save'"
+            v-model="user.admin" class="mt-3 mb-3">
+                Adminstrador?
+            </b-form-checkbox>
+            <b-row v-show="mode === 'save'">
                 <b-col md="6" sm="12">
                     <b-form-group label="Senha:" label-for="user-password">
                         <b-form-input id="user-password" type="password"
@@ -35,13 +40,32 @@
                     </b-form-group>
                 </b-col>
             </b-row>
+            <b-row>
+                <b-col xs="12">
+                    <b-button variant="primary" v-if="mode === 'save'"
+                    @click="save">Salvar</b-button>
+                    <b-button variant="danger" v-if="mode ==='remove'"
+                    @click="remove">Excluir</b-button>
+                    <b-button class="ml-2" @click="reset">Cancelar</b-button>
+                </b-col>
+            </b-row>
         </b-form>
-        <b-table hover striped :items="users" :fields="fields"></b-table>
+        <hr>
+        <b-table hover striped :items="users" :fields="fields">
+            <template slot="actions" slot-scope="data">
+                <b-button variant="warning" @click="loadUsers(data.item)" class="mr-2">
+                    <i class="fa fa-pencil"></i>
+                </b-button>
+                <b-buttom variant="danger" @click="loadUser(data.item, 'remove')">
+                    <i class="fa fa-trash"></i>
+                </b-buttom>
+            </template>
+        </b-table>
     </div>
 </template>
 
 <script>
-import {baseApiUrl} from '@/global'
+import {baseApiUrl, showError} from '@/global'
 
 export default {
     name: 'UserAdmin',
@@ -66,7 +90,35 @@ export default {
             axios.get(url).then(res => {
                 this.users = res.data
             })
-        }
+        },
+        reset() {
+            this.mode = 'save'
+            this.user = {}
+            this.loadUsers()
+        },
+        save() {
+            const method = this.user.id ? 'put' : 'post'
+            const id = this.user.id ? `/${this.user.id}` : ''
+            axios[method](`${baseApiUrl}/users${id}`, this.user)
+            .then(() => {
+                this.$toasted.global.defaultSuccess()
+                this.reset()
+            })
+            .catch(showError)
+        },
+        remove() {
+            const id = this.user.id
+            axios.delete(`${baseApiUrl}/users/${id}`)
+            .then(() => {
+                this.$toasted.global.defaultSuccess()
+                this.reset()
+            })
+            .catch(showError)
+        },
+        loadUser(user, mode = 'save') {
+            this.mode = mode
+            this.user = {...user}
+        } 
     },
     mounted() {
 
