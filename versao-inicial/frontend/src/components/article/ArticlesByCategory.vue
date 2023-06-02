55a2adc65864b3e5e73851c5b36dafe986efad7b
pedro-batista <peddrobatista2022@gmail.com>
2023-06-02 11:37:43 -0300

    Atualizando projeto...

--- versao-inicial/frontend/src/components/article/ArticlesByCategory.vue
+++ versao-inicial/frontend/src/components/article/ArticlesByCategory.vue
@@ -4,7 +4,7 @@
         :main="category.name" sub="Categoria"/>
         <ul>
             <li v-for="article in articles" :key="article.id">
-                {{ article.name }}
+                <ArticleItem :article="article"/>
             </li>
         </ul>
         <div class="load-more">
@@ -19,10 +19,11 @@
 import {baseApiUrl} from '@/global'
 import axios from 'axios'
 import PageTitle from '../template/PageTitle.vue'
+import ArticleItem from './ArticleItem.vue'
 
 export default {
     name: 'ArticlesByCategory',
-    components: {PageTitle},
+    components: {PageTitle, ArticleItem},
     data: function() {
         return {
             category: {},
@@ -46,6 +47,16 @@ export default {
             })
         }
     }, 
+    watch: {
+        $route(to) {
+            this.category.id = to.params.id
+            this.articles = []
+            this.page = 1
+            this.loadMore = true
+            this.getCategory()
+            this.getArticles()
+        }
+    },
     mounted() {
         this.category.id = this.$route.params.id
         this.getCategory()
